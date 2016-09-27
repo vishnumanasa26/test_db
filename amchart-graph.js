33397f3267851c0c13034e0a8825aec270aaf167
vishnumanasa26 <vishnu.manasa26@gmail.com>
2016-09-27 02:12:21 +0200

    with basic ngRoute for single page application

--- amchart-graph.js
+++ amchart-graph.js
@@ -1,51 +1,94 @@
 /**
- * Created by Nikhil Reddy on 20-09-2016.
- */
-/**
  * Created by Nikhil Reddy on 19-09-2016.
  */
 
 
-/*              <!--
- var chartData = generateChartData();
+var app = angular.module('chartdiv', ["ngRoute"]);
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when("/details",{
+            templateUrl : "in.html"
+        })
+        .when("/memory",{
+            templateUrl : ""
+        })
+        .when("/graphs",{
+            templateUrl : ""
+        })
+        .when("/pie",{
+            templateUrl : ""
+        });
+});
 
- function generateChartData() {
- var outData = [{"NAME":"DELL-PC","DATE":"Tue Sep  6 11:06:38 2016","LOAD":6,"MEMORY_TOTAL":0,"MEMORY":4948},{"NAME":"DELL-PC","DATE":"Tue Sep  6 11:04:52 2016","LOAD":5,"MEMORY_TOTAL":0,"MEMORY":5137},{"NAME":"DELL-PC","DATE":"Fri Sep 16 12:18:21 2016","LOAD":0,"MEMORY_TOTAL":8094,"MEMORY":3318},{"NAME":"DELL-PC","DATE":"Fri Sep 16 12:18:05 2016","LOAD":1,"MEMORY_TOTAL":8094,"MEMORY":3330}];
- var chartData = [];
+app.run(function($rootScope) {
+    $rootScope.hello = 'ROOT SCOPE';
+    $rootScope.memory = function () {
+        $rootScope.details1 = "root details";
+
+    }
+    $rootScope.details = function () {
+        $rootScope.write = "HELLOOO";
+    }
+});
 
 
- for (var i = 0; i < outData.length; i++) {
+app.controller('chart', function ($scope) {
+    $scope.value = "this is chart"
 
- var load = 6;
- var mem = outData[i].MEMORY;
-
- chartData.push({
-
- value: load
- //document.write(6);
- });
- }
+});
 
 
- return chartData;
- }
- */
+/*
 var chartData = generateChartData();
 
 function generateChartData() {
     var chartData = [];
-    var outData = [{"NAME":"DELL-PC","DATE":"Tue Sep  6 11:06:38 2016","LOAD":6,"MEMORY_TOTAL":0,"MEMORY":4948},{"NAME":"DELL-PC","DATE":"Tue Sep  6 11:04:52 2016","LOAD":5,"MEMORY_TOTAL":0,"MEMORY":5137},{"NAME":"DELL-PC","DATE":"Fri Sep 16 12:18:21 2016","LOAD":0,"MEMORY_TOTAL":8094,"MEMORY":3318},{"NAME":"DELL-PC","DATE":"Fri Sep 16 12:18:05 2016","LOAD":1,"MEMORY_TOTAL":8094,"MEMORY":3330},{"NAME":"DELL-PC","DATE":"Fri Sep 16 12:17:48 2016","LOAD":0,"MEMORY_TOTAL":8094,"MEMORY":3332},{"NAME":"DELL-PC","DATE":"Fri Sep 16 12:15:58 2016","LOAD":1,"MEMORY_TOTAL":8094,"MEMORY":3342},{"NAME":"DELL-PC","DATE":"Fri Sep 16 12:15:45 2016","LOAD":1,"MEMORY_TOTAL":8094,"MEMORY":3345},{"NAME":"DELL-PC","DATE":"Fri Sep 16 12:15:37 2016","LOAD":1,"MEMORY_TOTAL":8094,"MEMORY":3339},{"NAME":"DELL-PC","DATE":"Fri Sep 16 11:43:51 2016","LOAD":0,"MEMORY_TOTAL":8094,"MEMORY":3542},{"NAME":"DELL-PC","DATE":"Fri Sep 16 11:43:43 2016","LOAD":0,"MEMORY_TOTAL":8094,"MEMORY":3552},{"NAME":"DELL-PC","DATE":"Fri Sep 16 11:33:44 2016","LOAD":1,"MEMORY_TOTAL":8094,"MEMORY":3386},{"NAME":"DELL-PC","DATE":"Fri Sep 16 11:33:38 2016","LOAD":1,"MEMORY_TOTAL":8094,"MEMORY":3392},{"NAME":"DELL-PC","DATE":"Fri Sep 16 11:33:29 2016","LOAD":1,"MEMORY_TOTAL":8094,"MEMORY":3402},{"NAME":"DELL-PC","DATE":"Fri Sep 16 11:33:21 2016","LOAD":1,"MEMORY_TOTAL":8094,"MEMORY":3412},{"NAME":"DELL-PC","DATE":"Fri Sep 16 11:33:14 2016","LOAD":1,"MEMORY_TOTAL":8094,"MEMORY":3421},{"NAME":"DELL-PC","DATE":"Fri Sep 16 11:33:08 2016","LOAD":1,"MEMORY_TOTAL":8094,"MEMORY":3428},{"NAME":"DELL-PC","DATE":"Fri Sep 16 11:33:02 2016","LOAD":1,"MEMORY_TOTAL":8094,"MEMORY":3433},{"NAME":"DELL-PC","DATE":"Fri Sep 16 11:32:55 2016","LOAD":1,"MEMORY_TOTAL":8094,"MEMORY":3436},{"NAME":"DELL-PC","DATE":"Fri Sep 16 11:29:24 2016","LOAD":3,"MEMORY_TOTAL":8094,"MEMORY":3194},{"NAME":"DELL-PC","DATE":"Fri Sep 16 11:28:49 2016","LOAD":2,"MEMORY_TOTAL":8094,"MEMORY":3216},{"NAME":"DELL-PC","DATE":"Fri Sep 16 11:27:53 2016","LOAD":1,"MEMORY_TOTAL":8094,"MEMORY":3235},{"NAME":"DELL-PC","DATE":"Fri Sep 16 11:27:24 2016","LOAD":0,"MEMORY_TOTAL":8094,"MEMORY":3075},{"NAME":"DELL-PC","DATE":"Fri Sep 16 11:14:41 2016","LOAD":1,"MEMORY_TOTAL":8094,"MEMORY":3479},{"NAME":"DELL-PC","DATE":"Fri Sep 16 11:03:51 2016","LOAD":1,"MEMORY_TOTAL":0,"MEMORY":4062},{"NAME":"DELL-PC","DATE":"Fri Sep 16 10:08:01 2016","LOAD":2,"MEMORY_TOTAL":0,"MEMORY":3913},{"NAME":"DELL-PC","DATE":"Fri Sep 16 10:01:05 2016","LOAD":1,"MEMORY_TOTAL":0,"MEMORY":4383},{"NAME":"DELL-PC","DATE":"Fri Sep 16 09:51:49 2016","LOAD":1,"MEMORY_TOTAL":0,"MEMORY":4145},{"NAME":"DELL-PC","DATE":"Fri Sep 16 09:42:20 2016","LOAD":1,"MEMORY_TOTAL":0,"MEMORY":4743},{"NAME":"DELL-PC","DATE":"Fri Sep 16 09:42:13 2016","LOAD":1,"MEMORY_TOTAL":0,"MEMORY":4745},{"NAME":"DELL-PC","DATE":"Fri Sep 16 09:40:56 2016","LOAD":1,"MEMORY_TOTAL":0,"MEMORY":4616},{"NAME":"DELL-PC","DATE":"Fri Sep 16 09:40:50 2016","LOAD":1,"MEMORY_TOTAL":0,"MEMORY":4611},{"NAME":"DELL-PC","DATE":"Fri Sep 16 09:37:27 2016","LOAD":1,"MEMORY_TOTAL":0,"MEMORY":4809},{"NAME":"DELL-PC","DATE":"Fri Sep 16 09:21:05 2016","LOAD":1,"MEMORY_TOTAL":0,"MEMORY":4831},{"NAME":"DELL-PC","DATE":"Fri Sep 16 09:19:34 2016","LOAD":1,"MEMORY_TOTAL":0,"MEMORY":4808},{"NAME":"DELL-PC","DATE":"Fri Sep 16 09:18:34 2016","LOAD":0,"MEMORY_TOTAL":0,"MEMORY":4692}];
+    var num_month={"Jan":0,"Feb":1,"Mar":2,"Apr":3, "May":4,"Jun":5, "Jul":6,"Aug":7,"Sep":8,"Oct":9,"Nov":10,"Dec":11};
+    var outData = [{"NAME":"SRIHARSHA","DATE":"Wed Sep 21 13:04:43 2016","LOAD":0,"MEMORY_TOTAL":3084,"MEMORY":3086},{"NAME":"SRIHARSHA","DATE":"Wed Sep 21 13:03:20 2016","LOAD":0,"MEMORY_TOTAL":3023,"MEMORY":3024},{"NAME":"SRIHARSHA","DATE":"Wed Sep 21 13:03:14 2016","LOAD":1,"MEMORY_TOTAL":3033,"MEMORY":3032},{"NAME":"SRIHARSHA","DATE":"Wed Sep 21 13:02:49 2016","LOAD":1,"MEMORY_TOTAL":3029,"MEMORY":3029},{"NAME":"SRIHARSHA","DATE":"Wed Sep 21 13:02:40 2016","LOAD":0,"MEMORY_TOTAL":3026,"MEMORY":3027},{"NAME":"SRIHARSHA","DATE":"Wed Sep 21 13:02:31 2016","LOAD":0,"MEMORY_TOTAL":3056,"MEMORY":3057},{"NAME":"SRIHARSHA","DATE":"Wed Sep 21 13:02:15 2016","LOAD":0,"MEMORY_TOTAL":3091,"MEMORY":3091},{"NAME":"SRIHARSHA","DATE":"Wed Sep 21 13:02:02 2016","LOAD":1,"MEMORY_TOTAL":3149,"MEMORY":3149},{"NAME":"SRIHARSHA","DATE":"Wed Sep 21 13:01:47 2016","LOAD":0,"MEMORY_TOTAL":3264,"MEMORY":3265},{"NAME":"SRIHARSHA","DATE":"Sun Sep 18 16:43:33 2016","LOAD":1,"MEMORY_TOTAL":2803,"MEMORY":2804}];
+        //$.getJSON("http://localhost:8080/view");
+    //document.write(outData);
 
-    var firstDate = new Date(2012, 0, 1);
-    firstDate.setDate(firstDate.getDate() - 1000);
-    firstDate.setHours(0, 0, 0, 0);
+    //callEndpoint();
+    //[{"NAME":"SRIHARSHA","DATE":"Wed Sep 21 13:04:43 2016","LOAD":0,"MEMORY_TOTAL":3084,"MEMORY":3086},{"NAME":"SRIHARSHA","DATE":"Wed Sep 21 13:03:20 2016","LOAD":0,"MEMORY_TOTAL":3023,"MEMORY":3024},{"NAME":"SRIHARSHA","DATE":"Wed Sep 21 13:03:14 2016","LOAD":1,"MEMORY_TOTAL":3033,"MEMORY":3032},{"NAME":"SRIHARSHA","DATE":"Wed Sep 21 13:02:49 2016","LOAD":1,"MEMORY_TOTAL":3029,"MEMORY":3029},{"NAME":"SRIHARSHA","DATE":"Wed Sep 21 13:02:40 2016","LOAD":0,"MEMORY_TOTAL":3026,"MEMORY":3027},{"NAME":"SRIHARSHA","DATE":"Wed Sep 21 13:02:31 2016","LOAD":0,"MEMORY_TOTAL":3056,"MEMORY":3057},{"NAME":"SRIHARSHA","DATE":"Wed Sep 21 13:02:15 2016","LOAD":0,"MEMORY_TOTAL":3091,"MEMORY":3091},{"NAME":"SRIHARSHA","DATE":"Wed Sep 21 13:02:02 2016","LOAD":1,"MEMORY_TOTAL":3149,"MEMORY":3149},{"NAME":"SRIHARSHA","DATE":"Wed Sep 21 13:01:47 2016","LOAD":0,"MEMORY_TOTAL":3264,"MEMORY":3265},{"NAME":"SRIHARSHA","DATE":"Sun Sep 18 16:43:33 2016","LOAD":1,"MEMORY_TOTAL":2803,"MEMORY":2804}];
+
+    var our_date_json= outData[1].DATE;
+    //console.log(our_date_json);
+
+    var our_date_split= our_date_json.split(" ");
+    //console.log(parseInt(our_date_split[5]));
+    var month=our_date_split[1];
+    //console.log(num_month[month]);
+    //console.log(parseInt(our_date_split[3]));
+    var firstDate = new Date(parseInt(our_date_split[4]), num_month[month], parseInt(our_date_split[2]));
+    //console.log(firstDate.getDate());
+
+    //firstDate.setDate(firstDate.getDate() - 6);
+    //console.log(firstDate.getDate());
+    var our_time=our_date_split[3].split(":");
+    firstDate.setHours(parseInt(our_time[0]), parseInt(our_time[1]), parseInt(our_time[2]));
+    console.log(firstDate.getHours());
 
     for (var i = 0; i < outData.length; i++) {
+
+
+        our_date_json= outData[i].DATE;
+        //console.log(our_date_json);
+
+        our_date_split= our_date_json.split(" ");
+
+        our_time=our_date_split[3].split(":");
+        console.log(our_time);
         var newDate = new Date(firstDate);
-        newDate.setHours(0, i, 0, 0);
+        newDate.setHours(parseInt(our_time[0]), parseInt(our_time[1]), parseInt(our_time[2]));
+
+        //newDate.setHours(0, i, 0, 0);
+        console.log(newDate);
 
         var a =outData[i].LOAD;
         var b = outData[i].MEMORY;
+        var date_py=outData[i].DATE;
+
+      //  console.log(date_py);
 
         chartData.push({
             date: newDate,
@@ -82,81 +125,82 @@ var chart = AmCharts.makeChart("chartdiv", {
 
 
     panels: [{
-        showCategoryAxis: false,
-        title: "LLoad",
-        percentHeight: 70,
+    showCategoryAxis: false,
+    title: "LLoad",
+    percentHeight: 70,
 
+    stockGraphs: [{
+        id: "g1",
+        valueField: "value",
+        type: "smoothedLine",
+        lineThickness: 2,
+        bullet: "round"
+    }],
+
+
+    stockLegend: {
+        valueTextRegular: " ",
+        markerType: "none"
+    }
+},
+
+    {
+        title: "Memory",
+        percentHeight: 70,
         stockGraphs: [{
-            id: "g1",
-            valueField: "value",
+            valueField: "volume",
             type: "smoothedLine",
             lineThickness: 2,
             bullet: "round"
         }],
 
-
         stockLegend: {
             valueTextRegular: " ",
             markerType: "none"
         }
-    },
-
-        {
-            title: "Volume",
-            percentHeight: 70,
-            stockGraphs: [{
-                valueField: "volume",
-                type: "smoothedLine",
-                lineThickness: 2,
-                bullet: "round"
-            }],
-
-            stockLegend: {
-                valueTextRegular: " ",
-                markerType: "none"
-            }
-        }
-    ],
+    }
+],
 
     chartScrollbarSettings: {
-        graph: "g1",
+    graph: "g1",
         usePeriod: "10mm",
         position: "top"
-    },
+},
 
-    chartCursorSettings: {
-        valueBalloonsEnabled: true
-    },
+chartCursorSettings: {
+    valueBalloonsEnabled: true
+},
 
-    periodSelector: {
-        position: "top",
+periodSelector: {
+    position: "top",
         dateFormat: "YYYY-MM-DD JJ:NN",
         inputFieldWidth: 150,
         periods: [{
-            period: "hh",
-            count: 1,
-            label: "1 hour",
-            selected: true
+        period: "hh",
+        count: 1,
+        label: "1 hour",
+        selected: true
 
-        }, {
-            period: "hh",
-            count: 2,
-            label: "2 hours"
-        }, {
-            period: "hh",
-            count: 5,
-            label: "5 hour"
-        }, {
-            period: "hh",
-            count: 12,
-            label: "12 hours"
-        }, {
-            period: "MAX",
-            label: "MAX"
-        }]
-    },
+    }, {
+        period: "hh",
+        count: 2,
+        label: "2 hours"
+    }, {
+        period: "hh",
+        count: 5,
+        label: "5 hour"
+    }, {
+        period: "hh",
+        count: 12,
+        label: "12 hours"
+    }, {
+        period: "MAX",
+        label: "MAX"
+    }]
+},
 
-    panelsSettings: {
-        usePrefixes: true
-    }
+panelsSettings: {
+    usePrefixes: true
+}
 });
+*/
